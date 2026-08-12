da961a601f5cc733a5a4ecc0f9ccbc270f79683a
mb305151 <mb305151@student.polsl.pl>
2026-08-12 12:16:48 +0200

    feat: initial IaC setup with intentional storage vulnerability

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-zerotouch-sec-dev"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "stzerotouchsecdev001"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  # INTENTIONAL SECURITY VULNERABILITY (Bait for the scanner)
+  public_network_access_enabled   = true
+  allow_nested_items_to_be_public = true
+}
